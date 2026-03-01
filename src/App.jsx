5a1361b61470f40bbdca12ff571bbd1e3d03b9ef
DipuBaroi <dipubaroi758@gmail.com>
2026-03-01 15:04:37 +0600

    implement toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import AvailablePlayers from './components/AvailablePlayers/AvailablePlayers'
 import Navbar from './components/Navbar/Navbar'
+import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers'
 
 const fetchPlayer = async () => {
   const res = await fetch('./players.json')
@@ -11,15 +12,25 @@ const fetchPlayer = async () => {
 
 function App() {
 
+  const [toggle, setToggle] = useState(true)
+
   const playersPromise = fetchPlayer()
   return (
     <>
 
       <Navbar></Navbar>
-      
-      <Suspense fallback={<h2 className='text-center'>L O A D I N G <span className="loading loading-dots loading-xs"></span></h2>}>
+      <section className='w-11/12 mx-auto flex justify-between items-center'>
+          <h3 className='font-bold text-2xl'>Available Players</h3>
+          <div className='flex font-semibold'>
+              <button onClick={()=>setToggle(true)} className={`border border-gray-400 py-3 px-5 rounded-l-2xl border-r-0 ${toggle===true? ' bg-[#E7FE29]': ''}`}>Available</button>
+              <button onClick={()=>setToggle(false)} className={`border border-gray-400 py-3 px-5 rounded-r-2xl border-l-0 ${toggle===false? ' bg-[#E7FE29]' : ''}`}>Selected <span>(0)</span></button>
+          </div>
+      </section>
+      {
+        toggle === true?<Suspense fallback={<h2 className='text-center'>L O A D I N G <span className="loading loading-dots loading-xs"></span></h2>}>
         <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
-      </Suspense>
+      </Suspense>: <SelectedPlayers></SelectedPlayers>
+      }
     </>
   )
 }
--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const SelectedPlayers = () => {
     return (
-        <div>
-            
+        <div className='w-11/12 mx-auto'>
+            Selected players
         </div>
     );
 };
